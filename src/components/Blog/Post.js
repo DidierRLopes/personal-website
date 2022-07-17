0b9f876a19ee8dd881a0a81eb41e38749f83b3e0
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-07-17 16:28:48 +0100

    add my Blog to my website

---
+++ src/components/Blog/Post.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Post = ({ data }) => (
+  <div className="cell-container">
+    <article className="mini-post">
+      <header>
+        <h2 className="blue-text">
+          <a href={data.link}>{data.title.replace('&gt;&gt;', '>>')}</a>
+        </h2>
+        <p className="center">{data.pubDate.split(' ')[0]}</p>
+      </header>
+      <a href={data.link} className="image">
+        <div className="project-img">
+          <img
+            src={`${process.env.PUBLIC_URL}${data.thumbnail}`}
+            alt={data.title}
+          />
+        </div>
+      </a>
+    </article>
+  </div>
+);
+
+Post.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    pubDate: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Post;
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,20 +1,54 @@
-import React from 'react';
+/* const usePosts = posts.status === 'ok' && posts.items.map((post) => (
+  <div>
+    <h2>{post.title}</h2>
+    <p>{post.pubDate}</p>
+    <p>{post.link}</p>
+    <p>{post.description}</p>
+  </div>
+)); */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 
-const Blog = () => (
-  <Main title="Blog" description="Didier Rodrigues Lopes blog">
-    <article className="post" id="blog">
-      <header>
-        <div className="title">
-          <h2 data-testid="heading">
-            <Link to="/blog">Blog</Link>
-          </h2>
-        </div>
-      </header>
-    </article>
-  </Main>
-);
+import Post from '../components/Blog/Post';
+
+const Blog = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(
+        'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@dro-lopes',
+      );
+      const postsData = await response.json();
+      setPosts(postsData);
+    };
+    fetchPosts();
+  }, []);
+
+  const usePosts = posts.status === 'ok' && posts.items.map((post) => (
+    <Post data={post} key={post.title} />
+  ));
+
+  return (
+    <Main
+      title="Blog"
+      description="Didier Rodrigues Lopes's blog."
+    >
+      <article className="post" id="blog">
+        <header>
+          <div className="title">
+            <h2 data-testid="heading">
+              <Link to="/blog">Blog</Link>
+            </h2>
+            <p>Some of my written thoughts</p>
+          </div>
+        </header>
+        {usePosts}
+      </article>
+    </Main>
+  );
+};
 
 export default Blog;
