2ed2daaea7f340c3a13f489c50f7a7a09e7d4a38
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-12-18 02:04:54 +0000

    start converting css to tailwind

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -6,45 +6,50 @@ import ContactIcons from '../Contact/ContactIcons';
 const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
 
 const SideBar = () => (
-  <section id="sidebar">
-    <section id="intro">
-      <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.png`} alt="" />
+  <aside className="w-64 content-center text-center" aria-label="Sidebar">
+    <div className="overflow-y-auto py-4 rounded-xl dark:bg-gray-800">
+      <Link to="/">
+        <img
+          className="rounded-full mx-auto"
+          src={`${PUBLIC_URL}/images/me.png`}
+          alt="Didier"
+        />
       </Link>
-      <header>
-        <h2>Didier Lopes</h2>
-        <p>Founder and CEO @ OpenBB 🦋</p>
-      </header>
-    </section>
+      <div className="text-white my-4">
+        <p className="text-xl uppercase font-bold mb-2">Didier Lopes</p>
+        <p className="text-sm font-ligth mb-0">Founder and CEO</p>
+        <p className="text-sm font-ligth">OpenBB 🦋</p>
+      </div>
+    </div>
 
-    <section className="blurb">
-      <h2>CONTACT ME</h2>
-      <p>
-        <a href="https://cal.com/didierlopes/15min">
-          schedule a 15 min meeting
-        </a><br />
-        didier.lopes@openbb.finance
-      </p>
-      <ul className="actions">
-        <li>
-          {!window.location.pathname.includes('/resume') ? (
-            <Link to="/resume" className="button">
-              Learn More
-            </Link>
-          ) : (
-            <Link to="/" className="button">
-              About Me
-            </Link>
-          )}
-        </li>
-      </ul>
-    </section>
+    <div className="py-4 px-3 rounded-xl dark:bg-gray-400 text-white">
+      <p className="text-xl uppercase font-bold mb-2">REACH OUT</p>
+      <div className="dark:bg-gray-800 text-white font-light hover:text-black hover:bg-white py-2 px-4 border border-black hover:border-transparent content-center align-middle rounded">
+        <a
+          href="https://cal.com/didierlopes/15min"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          15 min meeting
+        </a>
+      </div>
+      <p className="text-sm font-ligth my-4">didier.lopes@openbb.finance</p>
+      {!window.location.pathname.includes('/resume') ? (
+        <Link to="/resume" className="w-120">
+          Learn More
+        </Link>
+      ) : (
+        <Link to="/" className="w-120">
+          About Me
+        </Link>
+      )}
+    </div>
 
-    <section id="footer">
+    <div className="w-120">
       <ContactIcons />
       <p className="copyright">&copy; Didier Rodrigues Lopes</p>
-    </section>
-  </section>
+    </div>
+  </aside>
 );
 
 export default SideBar;
