d93d4e47372abd6633069bf1952d3ebebc23b7bc
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-12-18 21:46:49 +0000

    smallfix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Main from './layouts/Main'; // fallback for lazy pages
 import './global.css'; // All of our styles
-
-const { PUBLIC_URL } = process.env;
 
 // Every route - we lazy load so that each page can be chunked
 // NOTE that some of these chunks are very small. We should optimize
@@ -17,7 +15,7 @@ const Stats = lazy(() => import('./pages/Stats'));
 const Books = lazy(() => import('./pages/Books'));
 
 const App = () => (
-  <Routes>
+  <Switch>
     <Route exact path="/" component={Main}>
       <Route path="/projects" component={Projects} />
       <Route path="/stats" component={Stats} />
@@ -27,7 +25,7 @@ const App = () => (
       { /* <Route component={NotFound} status={404} /> */ }
       <Route path="*" component={Index} />
     </Route>
-  </Routes>
+  </Switch>
 );
 
 export default App;
