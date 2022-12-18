c2a19c5c647a0c5f1e69ce5da092e606d29d20a5
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-12-18 21:52:18 +0000

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { lazy } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './layouts/Main'; // fallback for lazy pages
 import './global.css'; // All of our styles
+
+const { PUBLIC_URL } = process.env;
 
 // Every route - we lazy load so that each page can be chunked
 // NOTE that some of these chunks are very small. We should optimize
@@ -15,17 +17,26 @@ const Stats = lazy(() => import('./pages/Stats'));
 const Books = lazy(() => import('./pages/Books'));
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Main}>
-      <Route path="/projects" component={Projects} />
-      <Route path="/stats" component={Stats} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/resume" component={Resume} />
-      <Route path="/books" component={Books} />
-      { /* <Route component={NotFound} status={404} /> */ }
-      <Route path="*" component={Index} />
-    </Route>
-  </Switch>
+  <BrowserRouter basename={PUBLIC_URL}>
+    <Suspense fallback={<Main />}>
+      <div className="bg-indigo-900 text-center py-4 lg:px-4">
+        <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+          <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">NOTE3</span>
+          <span className="font-semibold mr-2 text-left flex-auto">Currently learning frontend and converting css to Tailwind</span>
+        </div>
+      </div>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/stats" component={Stats} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/books" component={Books} />
+        <Route component={NotFound} status={404} />
+        { /* <Route path="*" component={Index} /> */ }
+      </Switch>
+    </Suspense>
+  </BrowserRouter>
 );
 
 export default App;
