b7c4cc86335a72204d71149bd0f5d96c704a4e5a
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-12-18 21:58:29 +0000

    c

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ const Books = lazy(() => import('./pages/Books'));
 
 const App = () => (
   <BrowserRouter basename={PUBLIC_URL}>
-    <Suspense fallback={<Main />}>
-      <div className="bg-indigo-900 text-center py-4 lg:px-4">
-        <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-          <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">NOTE3</span>
-          <span className="font-semibold mr-2 text-left flex-auto">Currently learning frontend and converting css to Tailwind</span>
-        </div>
+    <div className="bg-indigo-900 text-center py-4 lg:px-4">
+      <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+        <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">NOTE</span>
+        <span className="font-semibold mr-2 text-left flex-auto">Currently learning frontend and converting css to Tailwind</span>
       </div>
+    </div>
+    <Suspense fallback={<Main />}>
       <Switch>
         <Route exact path="/" component={Index} />
         <Route path="/projects" component={Projects} />
@@ -33,7 +33,6 @@ const App = () => (
         <Route path="/resume" component={Resume} />
         <Route path="/books" component={Books} />
         <Route component={NotFound} status={404} />
-        { /* <Route path="*" component={Index} /> */ }
       </Switch>
     </Suspense>
   </BrowserRouter>
