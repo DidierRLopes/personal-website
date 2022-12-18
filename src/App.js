66e782c724b855249759f9236d93b7c84c53ba9a
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-12-18 21:43:22 +0000

    test

--- src/App.js
+++ src/App.js
@@ -17,26 +17,17 @@ const Stats = lazy(() => import('./pages/Stats'));
 const Books = lazy(() => import('./pages/Books'));
 
 const App = () => (
-  <BrowserRouter basename={PUBLIC_URL}>
-    <Suspense fallback={<Main />}>
-      <div className="bg-indigo-900 text-center py-4 lg:px-4">
-        <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-          <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">NOTE2</span>
-          <span className="font-semibold mr-2 text-left flex-auto">Currently learning frontend and converting css to Tailwind</span>
-        </div>
-      </div>
-      <Switch>
-        <Route exact path="/" component={Index} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/stats" component={Stats} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/books" component={Books} />
-        { /* <Route component={NotFound} status={404} /> */ }
-        <Route path="*" component={Index} />
-      </Switch>
-    </Suspense>
-  </BrowserRouter>
+  <Routes>
+    <Route exact path="/" component={Main}>
+      <Route path="/projects" component={Projects} />
+      <Route path="/stats" component={Stats} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/resume" component={Resume} />
+      <Route path="/books" component={Books} />
+      { /* <Route component={NotFound} status={404} /> */ }
+      <Route path="*" component={Index} />
+    </Route>
+  </Routes>
 );
 
 export default App;
