f86a856d8204bfdb71ad01c45a2ecf3993e0d525
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-07-02 19:37:56 -0700

    update blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 // Get the components associated with this page
 import BlogSection from '../components/Blog/Blog';
-import RecommendationSection from '../components/Blog/Favourites';
 
 // Get the data necessary for this page
-import favouritesData from '../data/blog/favourites';
 
 // Blog page
 const Blog = () => (
