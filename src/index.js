02b18ca364c41e2fd326476d831819f9cf281669
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-07-02 20:16:16 -0700

    order imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import BrowserRouter from 'react-router-dom/BrowserRouter'
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
+import App from './App';
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
