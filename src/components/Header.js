3663773dac852b2687ba91b62e75d23a03c8b289
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-07-15 23:26:49 -0700

    start overhaul website according to Meg design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import Navbar from './Navbar';
+import routes from '../data/routes';
+import LetterDIcon from '../icons/LetterDIcon';
+import '../global.css';
 
 const Header = () => (
-  <header className="sticky top-0 z-50 bg-blue-200">
-    <div className="container flex flex-wrap items-center justify-between mx-auto">
-      <a
-        href="/"
-        className="block py-2 pl-3 pr-4 text-black font-semibold rounded md:border-0 md:hover:text-blue-700"
-      >
-        Didier Rodrigues Lopes
-      </a>
-      <Navbar />
-    </div>
-  </header>
+  <section className="header">
+    <header className="sticky top-0 z-50">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <a
+          href="/"
+          className="flex hover:text-[#FF8C00]"
+        >
+          <LetterDIcon />
+          <div className="ml-2 mt-1">
+            Didier
+          </div>
+        </a>
+        <nav className="px-2 sm:px-4 py-2.5 rounded">
+          <div className="hidden w-full md:block md:w-auto">
+            <ul className="flex flex-col p-4 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+              {routes.slice(1).map((route) => (
+                <li className="hover:text-[#FF8C00]">
+                  <a href={`/#${route.path}`}>{route.label}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  </section>
 );
 
 export default Header;
