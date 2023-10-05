db9418a0edc9d9cc3f637c6316020c30daafb25e
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-10-04 23:08:39 -0700

    Update books2read.js

--- src/data/books/books2read.js
+++ src/data/books/books2read.js
@@ -343,7 +343,7 @@ const books2read = [
     title: 'The Rational Optimist: How Prosperity Evolves',
     author: 'Matt Ridley',
     image: 'https://m.media-amazon.com/images/I/61BwDm28ViL._SL1500_.jpg',
-  }
+  },
 ];
 
 export default books2read;
